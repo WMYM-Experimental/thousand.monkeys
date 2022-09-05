e0455b1078e239f5a5ffad8c11562b8cb7a3d4e4
WMYM <washington.yandun@udla.edu.ec>
2022-09-04 23:39:58 -0500

    add some general styles

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,31 +1,30 @@
-// import info css file
-import './Info.css';
+import "./Info.css";
 
 export const Info = () => {
     return (
         <>
-            <div className="wrapper__info">
-                <div className="info__header">
+            <div className="wrapper__info glass">
+                <div className="info__header glass">
                     <h1 className="info_title">Thousand Monkeys Algo</h1>
                     <h2 id="best">The best Phrase</h2>
                 </div>
 
-                <div className="info__properties">
+                <div className="info__properties glass">
                     <p className="info__text">
-                        total-genrations:
-                        <span id="total-genrations">0</span>
+                        Total-genrations:
+                        <span id="total-genrations"> 0 </span>
                     </p>
                     <p className="info__text">
-                        average-fitness:
-                        <span id="average-fitness">0</span>
+                        Average-fitness:
+                        <span id="average-fitness"> 0 </span>
                     </p>
                     <p className="info__text">
-                        population:
-                        <span id="population">0</span>
+                        Population:
+                        <span id="population"> 0 </span>
                     </p>
                     <p className="info__text">
-                        mutation-rate:
-                        <span id="mutation-rate">0</span>
+                        Mutation-rate:
+                        <span id="mutation-rate"> 0 </span>
                     </p>
                 </div>
             </div>
--- src/components/show/Show.tsx
+++ src/components/show/Show.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import "./Show.css";
 
 export const Show = () => {
     return (
         <>
-            <div id="show">
+            <div id="show" className="glass">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Cupiditate labore voluptate debitis enim veniam doloribus
                 architecto sunt, placeat possimus ex eum ipsam itaque qui
